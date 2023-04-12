5b2c456d784f3fecfd978a3fd3d886b5e0302181
Kazi Solayman <kazisolayman123@gmail.com>
2023-04-12 23:00:26 +0600

    chart added

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -8,7 +8,22 @@ const Blogs = () => {
                 <h2>Blogs</h2>
             </div>
             <div className="blog-section">
-                <h5>What is React?</h5>
+                <div>
+                    <h5>When should you use context API?</h5>
+                    <p>Ans: Context is primarily used when some data needs to be accessible by many components at different nesting levels. Apply it sparingly because it makes component reuse more difficult. If you only want to avoid passing some props through many levels, component composition is often a simpler solution than context.</p>
+                </div>
+                <div>
+                    <h5>What is a custom hook?</h5>
+                    <p>Ans: A custom hook is a special JavaScript function whose name starts with 'use' and can be used to call other hooks. Let's take a look at some major differences between a custom React JS hook and React JS components: A custom hook does not require a specific signature.</p>
+                </div>
+                <div>
+                    <h5>What is useRef?</h5>
+                    <p>Ans: useRef is a built-in React hook that accepts one argument as the initial value and returns a reference. A reference is an object having a special property current .</p>
+                </div>
+                <div>
+                    <h5>What is useMemo?</h5>
+                    <p>Ans: useMemo is a React Hook that lets you cache the result of a calculation between re-renders. const cachedValue = useMemo(calculateValue, dependencies) Reference. useMemo(calculateValue, dependencies)</p>
+                </div>
             </div>
         </div>
     );
--- src/components/Statics/Statics.jsx
+++ src/components/Statics/Statics.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ComposedChart, BarChart, Bar } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ComposedChart, BarChart, Bar, PieChart, Pie, RadialBarChart, RadialBar } from 'recharts';
 import './Statics.css'
 
 const Statics = () => {
@@ -9,42 +9,50 @@ const Statics = () => {
             id: 1,
             assignment: 'Assignment-1',
             marks: 60,
+            "fill": "#8884d8"
         },
         {
             id: 2,
             assignment: 'Assignment 2',
             marks: 60,
+            "fill": "#83a6ed"
         },
         {
             id: 3,
             assignment: 'Assignment 3',
             marks: 60,
+            "fill": "#8dd1e1"
         },
         {
             id: 4,
             assignment: 'Assignment 4',
             marks: 58,
+            "fill": "#82ca9d"
         },
         {
             id: 5,
             assignment: 'Assignment 5',
             marks: 50,
+            "fill": "#a4de6c"
         },
         {
             id: 6,
             assignment: 'Assignment 6',
-            marks: 47
+            marks: 47,
+            "fill": "#d0ed57"
             ,
         },
         {
             id: 7,
             assignment: 'Assignment 7',
             marks: 60,
+            "fill": "#ffc658"
         },
         {
             id: 8,
             assignment: 'Assignment 8',
             marks: 52,
+            "fill": "#a4de6c"
         }
     ]
 
@@ -52,16 +60,19 @@ const Statics = () => {
         <div className='chart-section'>
             <h2>Assignments Marks</h2>
             <div>
-                <BarChart
-                    width={1100}
-                    height={300}
-                    data={assignmentsData}
-                >
-                    <Bar type="monotone" dataKey="marks" fill="#8884d8"></Bar>
-                    <XAxis dataKey="assignment"></XAxis>
-                    <YAxis></YAxis>
-                    <Tooltip></Tooltip>
-                </BarChart>
+            <RadialBarChart 
+                    width={730} 
+                    height={650} 
+                    innerRadius="10%" 
+                    outerRadius="80%" 
+                    data={assignmentsData} 
+                    startAngle={180} 
+                    endAngle={0}
+                    >
+                    <RadialBar minAngle={15} label={{ fill: '#8884d8', position: 'insideStart' }} background clockWise={true} dataKey='marks' />
+                    
+                    <Tooltip />
+            </RadialBarChart>
             </div>
         </div>
     );
